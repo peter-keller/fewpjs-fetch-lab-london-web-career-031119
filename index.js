3bdebd89fb79397b8d420da317411681d168e2b5
peter-keller <keller.f.peter@gmail.com>
2019-04-23 15:59:33 +0100

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector("main");
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener("DOMContentLoaded", function() {
+  fetchBooks();
+});
